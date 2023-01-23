ca2b3aebfbe9ee8420017a1518053c4d4f41083b
Martin Lippert <mlippert@gmail.com>
2023-01-23 18:33:26 +0100

    initial spring boot project with outdated mysql connector

---
+++ src/main/java/com/example/m2eexceptionsample/M2eExceptionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2eexceptionsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2eExceptionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2eExceptionSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m2eexceptionsample/M2eExceptionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2eexceptionsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2eExceptionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
